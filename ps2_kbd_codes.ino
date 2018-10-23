6a8bb815ad081c80f72c3352c88f0c41c6910541
Brillo-Solutions <brillosolutions@gmail.com>
2018-10-23 06:32:47 -0700

    v1.0.1 - Output in both BIN and HEX

--- ps2_kbd_codes.ino
+++ ps2_kbd_codes.ino
@@ -1,20 +1,20 @@
 /*Written By: Er. Dapinder Singh Virk
   Email ID: brillosolutions@gmail.com
-  Version: 1.0.0
+  Version: 1.0.1
   Date: October 20, 2018
   Outputs: Scan and break codes on serial terminal.*/
 
-#define clkPin 3
-#define dataPin 4
+#define clkLine 3
+#define dataLine 4
 uint8_t kbdBits[33], i = 0;
 
 void setup() 
 {
   // put your setup code here, to run once:
-  pinMode(clkPin, INPUT);
-  pinMode(dataPin, INPUT);
-  Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(clkPin), kbdInterrupt, FALLING);
+  pinMode(clkLine, INPUT);
+  pinMode(dataLine, INPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(clkLine), kbdInterrupt, FALLING);
 }
 
 void loop() 
@@ -23,11 +23,14 @@ void loop()
   if (i == 33)
   {
     i = 0;
+    Serial.print("BIN: ");
+    for (int j = 0; j <= 32; j++)
+      Serial.print(kbdBits[j]);
+    Serial.print("\nHEX: ");
     uint8_t kbdByte = 0, uprLimit = 8, lwrLimit = 1;
     for (int j = 0; j <= 2; j++)
     {
       for (int k = uprLimit; k >= lwrLimit; k--)
-      {
         if (kbdBits[k])
         {
           kbdByte <<= 1;
@@ -35,17 +38,15 @@ void loop()
         }
         else
           kbdByte <<= 1;
-      }
       Serial.print(kbdByte, HEX);
       uprLimit += 11;
       lwrLimit += 11;
     }
-    Serial.print("\r\n");
+    Serial.print("\r\n\n");
   }
 }
 
 void kbdInterrupt()
 {
-  kbdBits[i] = digitalRead(dataPin);
-  i++;
+  kbdBits[i++] = digitalRead(dataLine);
 }
